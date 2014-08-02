a5483e86b7049a940bc983ced81ba45911856740
smanoh <smo304@nyu.edu>
2014-08-02 15:08:32 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='MCZ_err',
+      version='1.0',
+      description='MC simulation for getting metallicity confidence region',
+      author='Seung Man Oh',
+      author_email='smo304@nyu.edu',
+      py_modules=['metallicity','MCZ']
+     )
