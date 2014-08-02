f41c6c23047ac28560dcb9d0072903e795d73e64
smanoh <smo304@nyu.edu>
2014-08-02 23:29:14 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-import os
-from setuptools import setup
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+from distutils.core import setup
 
 setup(name='MCZ',
       version='1.0',
