747a73a2f59ec3d4a404d2962d125af05b39a583
fedhere <fedhere@gmail.com>
2014-11-17 02:30:01 -0500

    bug fixes, removed saved gaussian plot, removed useless gaussian function, added bayesian method for histogram binsize, fixed bug in calculating percentiles, improved apparence of plots, added plotting formatting code, improved output added pickling of the simulation and option to read the pickled saved simulation

--- MCZ/MCZ_err.py
+++ MCZ/MCZ_err.py
@@ -1,15 +1,28 @@
+import os,sys,argparse,warnings
+
 import numpy as np
+import scipy.stats as stats
 import matplotlib.pyplot as plt
-import scipy.stats.mstats as ssm
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+
+#modules of this package
+import pylabsetup
 import metallicity
-import os
-from matplotlib.ticker import FuncFormatter
-import warnings
-import sys, argparse
+
+#pickle may not be installed
+NOPICKLE=False
+try:
+    import pprint, pickle
+except:
+    NOPICKLE=True
+
 
 CLOBBER=False
 VERBOSE=False
+UNPICKLE=False
+RUNSIM=True
 BINMODE='t'
+binning={'bb':'Bayesian blocks','d':"Doane's formula",'s':r'$\sqrt{N}$','t':r'$2 N^{1/3}$'}
 
 ##############################################################################
 ##Reads the flux file and returns it as an array.
@@ -43,12 +56,6 @@ def readfile(filename):
     return b,j
 
 ##############################################################################
-##a simple gaussian
-##############################################################################
-def gaussian(x,m,s):
-    return 1./(s*np.sqrt(2*np.pi)) * np.exp(-(x-m)**2 /(2 *s**2))
-
-##############################################################################
 ##returns appropriate bin size for the number of data
 ##mode 'd' calculates this based on Doane's formula 
 ##mode 's' calculates this based on sqrt of number of data
@@ -56,20 +63,21 @@ def gaussian(x,m,s):
 ##############################################################################
 def getbinsize(n,data,):
     if BINMODE=='d':
-        g1=ssm.moment(data,moment=3)
-        s1=np.sqrt(6.*(n-2.)/((n+1.)*(n+3.)))
-        k=1+np.log2(n)+np.log2(1+np.abs(g1/s1))
+        g1=stats.mstats.moment(data,moment=3)
+        s1=np.sqrt(float(n)/6.0)
+        #s1=1.0/np.sqrt(6.*(n-2.)/((n+1.)*(n+3.)))
+        k=1+np.log(n)+np.log(1+(g1*s1))
     elif BINMODE=='s':
         k=np.sqrt(n)
     elif BINMODE=='t':
         k=2.*n**(1./3.)
     return k
+
 ##############################################################################        
-##almost (symmetric) method
 ##estimating error starting at the peak.
-##almost (symmetric) - make it go same l and r
+##(almost) symmetric - make it go same l and r
 ##not well behaved in case of multiple peaks
-##code not up to date(oct 21 2013)
+##DEPRECATED
 ##############################################################################        
 def err_est1(count,prob=0.68):
     peak=np.argmax(count)
@@ -87,50 +95,16 @@ def err_est1(count,prob=0.68):
 
     return peak-l,peak-r,(total-temp)/total,0,0
 
-##############################################################################
-##(central) method
-#estmating error starting from the ends.
-##input: 'count' returned from plt.hist, 'prob' default 68%
-##output: the index of left boundary, right boundary, confidence,
-#fraction to be shifted to left, fraction to be shifted from the right
-##############################################################################
-def err_est(count,prob=0.68):
-    total=np.sum(count)
-    l=0
-    fl=0.
-    r=len(count)-1
-    fr=0.
-    ltemp=count[l]
-    rtemp=count[r]
-    
-    thresh=total*(1.-prob)/2.
-    while ltemp<=thresh:
-        l+=1
-        ltemp+=count[l]
-        if ltemp>thresh:
-            ltemp-=count[l]
-            fl=(thresh-ltemp)/count[l]   
-            l-=1
-            break
-    while rtemp<=thresh:
-        r-=1
-        rtemp+=count[r]
-        if rtemp>thresh:
-            rtemp-=count[r]
-            fr=(thresh-rtemp)/count[r]
-            r+=1
-            break
-    return l,r,(total-2*thresh)/total,fl,1-fr    
 
 ##############################################################################
 ##Save the result as histogram as name
 ## delog - if true de-logs the data. False by default
 ##############################################################################
-def savehist(data,filename,Zs,nsample,i,path,delog=False):
+def savehist(data,snname,Zs,nsample,i,path,nmeas,delog=False):
     
     global CLOBBER
-    name='%s_n%d_%s_i%d'%((filename,nsample,Zs,i))
-
+    
+    name='%s_n%d_%s_%d'%((snname,nsample,Zs,i+1))
     outdir=os.path.join(path,'hist')
     outfile=os.path.join(outdir,name+".pdf")
     if os.path.isfile(outfile) and not CLOBBER:
@@ -139,64 +113,76 @@ def savehist(data,filename,Zs,nsample,i,path,delog=False):
         CLOBBER =True
 
     plt.clf()
-
     ###de-log###
     if delog:
         with np.errstate(invalid='ignore'):
             data=np.power(10,np.absolute(data-12))
         
-    data=data[np.isfinite(data)]
-    #    data=np.sort(data)
-    
     ####kill outliers###
-
-    mean=np.mean(data)
-    std=np.std(data)
-    data=data[(data>mean-5*std)*(data>mean-5*std)][:nsample]
-
+    data=data[np.isfinite(data)]
+    data,ignore,ignore=stats.sigmaclip(data,high=5.0,low=5.0)
     n=data.shape[0]
     
     if data.shape[0]<=0:
         print name,'is blank' ##if no data
         return "-1, -1, -1"    
     try:
-        median=np.median(data)
-        ######find fit and save hist######
+        ###find C.I.###
+        median,pc16,pc84=np.percentile(data,[50,16,84])
+        std=np.std(data)
+        left=pc16
+        right=pc84
+        maxleft=median-std*5
+        maxright=median+std*5
+
+        ######histogram######
         ###find appropriate bin size###
-        numbin=getbinsize(data.shape[0],data)
-        
-        ###make hist###
-        count, bins, ignored = plt.hist(data, numbin, normed=1.0,color=['steelblue'])
-
-        #####FED
-        to_unity = lambda y, pos:  "%.2f"%(y / float(max(count)))
-        plt.gca().yaxis.set_major_formatter(FuncFormatter(to_unity))
-
-        ###find error###
-        l,r,t,fl,fr=err_est(count)
-        y=np.zeros(len(bins))
-        left=bins[l]+(bins[l+1]-bins[l])*fl
-        right=bins[r]-(bins[r]-bins[r-1])*fr
-        if right<median or left>median:
-            median = (left+right)/2
-        ###plot hist###
-        plt.plot(bins,y)
-        plt.xlim(8.6,9.3)
-        plt.axvspan(left,right,color='red',alpha=0.4)
-        st='%s i=%d\nn=%d\nconfidence: %.2f\nmedian: %.3f\n16th Percentile: %.3f\n84th Percentile: %.3f'%(Zs,i,n,t,round(median,3),round(left,3),round(right,3))
-        plt.annotate(st, xy=(0.60, 0.70), xycoords='axes fraction',fontsize=15)
-        if delog:
-            plt.xlabel('O/H',fontsize=18)
+        ##if astroML is available use it to get Bayesian blocks
+        if BINMODE=='bb':
+            try:
+                from astroML.plotting import hist as amlhist
+                distrib=amlhist(data, bins='blocks', normed=True)
+                plt.clf()
+            except:
+                print "bayesian blocks for histogram requires astroML to be installed"
+                print "defaulting to 2*n**1/3 "
+                ##otherwise 
+                numbin=getbinsize(data.shape[0],data)        
+                distrib=np.histogram(data, numbin, density=True)
         else:
-            plt.xlabel('12+log(O/H)',fontsize=18)
-        plt.ylabel('counts',fontsize=18)
-        plt.axvline(x=median,linewidth=2,color='black',ls='--')
-        plt.savefig(outfile,clobber=False,format='pdf')
+            numbin=getbinsize(data.shape[0],data)        
+            distrib=np.histogram(data, numbin, density=True)            
+        ###make hist###
+        counts, bins=distrib[0],distrib[1]
+        widths=np.diff(bins)
+        countsnorm=counts/np.max(counts)
+
+        ###plot hist###
+        plt.bar(bins[:-1],countsnorm,widths,color=['gray'])
+        plt.minorticks_on()
+        plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+        plt.xlim(maxleft,maxright)
+        plt.ylim(0,1.15)
+        plt.yticks(np.arange(0.2,1.1,0.2 ), [ "%.1f"%x for x in np.arange(0.2,1.1,0.2)])  
+        plt.axvspan(left,right,color='DarkOrange',alpha=0.4)
+        plt.axvline(x=median,linewidth=2,color='white',ls='--')
+        st='%s '%(snname)
+        plt.annotate(st, xy=(0.13, 0.6), xycoords='axes fraction',fontsize=18,fontweight='bold')
+        st='%s '%(Zs)
+        plt.annotate(st, xy=(0.62, 0.93), xycoords='axes fraction',fontsize=18,fontweight='bold')
+        st='measurement %d of %d\n\nmedian: %.3f\n16th Percentile: %.3f\n84th Percentile: %.3f\n\n'%(i+1,nmeas,round(median,3),round(left,3),round(right,3))
+        plt.annotate(st, xy=(0.62, 0.56), xycoords='axes fraction',fontsize=18)
+        st='MC sample size %d\nhistogram rule: %s'%(nsample,binning[BINMODE])
+        plt.annotate(st, xy=(0.62, 0.55), xycoords='axes fraction',fontsize=13)
+        if delog:
+            plt.xlabel('O/H')
+        else:
+            plt.xlabel('12+log(O/H)')
+        plt.ylabel('relative counts')
+        plt.savefig(outfile,format='pdf')
         
         ###print out the confidence interval###
-#        print name, ':\t%f +- %f'%((left+right)/2.,(right-left)/2.)
-
-        print '{0:40} {1:>13.3f} - {2:>7.3f} + {3:>7.3f}'.format(name, round(median,3), round(median-left,3), round(right-median,3))
+        print '{0:15} {1:20} {2:>13.3f} - {3:>7.3f} + {4:>7.3f}'.format(snname, Zs, round(median,3), round(median-left,3), round(right-median,3))
 
         return "%f, %f, %f"%(round(median,3), round(median-left,3), round(right-median,3))
 
@@ -216,83 +202,88 @@ def savehist(data,filename,Zs,nsample,i,path,delog=False):
 ##      mode 's' calculates this based on sqrt of number of data
 ##      mode 't' calculates this based on 2*n**1/3 (default)
 ##############################################################################
-def run((filename, flux, err, path), nsample,delog=False):
-    ###flux and err must be same dimensions
+def run((name, flux, err, path), nsample,delog=False, unpickle=False):
+    global RUNSIM
+    assert(flux.shape == err.shape), "flux and err must be same dimensions" 
+
     newnsample=int(nsample+0.1*nsample)
     p=os.path.join(path,'..')
-    if flux.shape != err.shape:
-        print "flux and err must be of same dimensions"
-        return
     nm = flux.shape[1]
 
     ###retrieve the metallicity keys
     Zs= metallicity.get_keys()
 
     ###make necessary paths
-    if not os.path.exists(os.path.join(p,'bins','%s'%filename)):
-        os.makedirs(os.path.join(p,'bins','%s'%filename))
-    if not os.path.exists(os.path.join(p,'bins','%s'%filename,'hist')):
-        os.makedirs(os.path.join(p,'bins','%s'%filename,'hist'))
-    binp=os.path.join(p,'bins','%s'%filename)
+    if not os.path.exists(os.path.join(p,'output','%s'%name)):
+        os.makedirs(os.path.join(p,'output','%s'%name))
+    if not os.path.exists(os.path.join(p,'output','%s'%name,'hist')):
+        os.makedirs(os.path.join(p,'output','%s'%name,'hist'))
+    binp=os.path.join(p,'output','%s'%name)
+    picklefile=os.path.join(binp,'%s_n%d.pkl'%(name,nsample))
 
     if VERBOSE: print "output files will be stored in ",binp
 
-    ###Sample 'nsample' points from a gaussian###
-    ##a gaussian centered on 0 with std 1
-    mu=0
-    sigma=1
-    sample=np.random.normal(mu,sigma,newnsample)
-    
-    ###save this sampled gaussians into a png file###
-    count, bins, ignored = plt.hist(sample, 40,normed=1)
-    plt.plot(bins,gaussian(bins,mu,sigma))
-    plt.title("Sampled")
-    st="n=%d"%newnsample
-    plt.annotate(st, xy=(0.70, 0.85), xycoords='axes fraction')
+    if unpickle:
+        RUNSIM=False
+        if not os.path.isfile(picklefile):
+            print "missing pickled file for this simulation: name, nsample.\nrun the MonteCarlo? Ctr-C to exit, Return to continue?\n"
+            raw_input()
+            RUNSIM=True
+        else:
+            pklfile = open(picklefile, 'rb')
+            res=pickle.load(pklfile)
 
-    plt.savefig(os.path.join(binp,'%s_n%d_sample.pdf'%(filename,newnsample)),format='pdf')
-    plt.clf()
-
-    
-    ###Start calculation###
-    ## the flux to be feed to the calculation will be
-    ## flux + error*i
-    ## where i is the sampled gaussian    
-    if VERBOSE: print "Starting iteration"
-
-    #initialize the dictionary
-    res={}
-    for key in Zs:
-        res[key]=[]
-
-    #do the iterations
-    for i in range(newnsample):
-        temp=flux+err*sample[i]
-        warnings.filterwarnings("ignore")
-        t=metallicity.calculation(temp,nm,disp=VERBOSE)
-        for key in Zs:
-            res[key].append(t[key])
-            
-    #make the result a np.array
-    for key in Zs:
-        res[key]=np.array(res[key])
+    if RUNSIM:
+        ###Sample 'nsample' points from a gaussian centered on 0 with std 1
+        mu=0
+        sigma=1
+        sample=np.random.normal(mu,sigma,newnsample)
         
-    if VERBOSE: print "Iteration Complete"
-    
-    ###Bin the results and save the uncertainty###
-    print '{0:40} {1:>13} - {2:>7} + {3:>7}'.format("diagnostic", "metallicity","left", "right")
-    for i in range(nm):
-        fi=open(os.path.join(binp,'%s_n%d_i%d.csv'%(filename,nsample,i)),'w')
-        fi.write("%s, Median (Z), Left, Right\n"%filename)
-
-        print "measurement %d-------------------------------------------------------------"%i
-
+        
+        ###Start calculation###
+        ## the flux to be feed to the calculation will be
+        ## flux + error*i
+        ## where i is the sampled gaussian    
+        if VERBOSE: print "Starting iteration"
+        
+        #initialize the dictionary
+        res={}
         for key in Zs:
-            s=key+", "+savehist(res[key][:,i],filename,key,nsample,i,binp,delog=delog)+'\n'
+            res[key]=[]
+            
+        #do the iterations
+        for i in range(newnsample):
+            temp=flux+err*sample[i]
+            warnings.filterwarnings("ignore")
+            t=metallicity.calculation(temp,nm,disp=VERBOSE)
+            for key in Zs:
+                res[key].append(t[key])
+            
+        #recast the result into np.array
+        for key in Zs:
+            res[key]=np.array(res[key])
+        
+        if VERBOSE: print "Iteration Complete"
+    
+        #"I CAN PICKLE THIS!"
+        #pickle this realization
+        if not NOPICKLE:
+            pickle.dump(res,open(picklefile,'wb'))
+
+    ###Bin the results and save###
+    print '{0:15} {1:20} {2:>13} - {3:>7} + {4:>7} {5:11} {6:>7}'.format("SN","diagnostic", "metallicity","34%", "34%", "(sample size:",'%d)'%nsample)
+    for i in range(nm):
+        fi=open(os.path.join(binp,'%s_n%d_%d.csv'%(name,nsample,i+1)),'w')
+        fi.write("%s, Median (Z), Left, Right\n"%name)
+        
+        print "\n\nmeasurement %d-------------------------------------------------------------"%(i+1)
+        
+        for key in Zs:
+            s=key+", "+savehist(res[key][:,i],name,key,nsample,i,binp,nm,delog=delog)+'\n'
             fi.write(s)
         fi.close()
     
-    if VERBOSE: print "uncertainty calculation complete"
+        if VERBOSE: print "uncertainty calculation complete"
 
 ##############################################################################
 ##The input format generator
@@ -342,12 +333,14 @@ def in_mm(filename, path):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', metavar='<filename>', type=str, help="the common filename")
-    parser.add_argument('nsample', metavar='N', type=int, help="number of iterations")
+    parser.add_argument('name', metavar='<name>', type=str, help="the SN file name (root of the _min,_max file names")
+    parser.add_argument('nsample', metavar='N', type=int, help="number of iterations, minimum 100")
     parser.add_argument('--clobber',default=False, action='store_true', help="replace existing output")
     parser.add_argument('--delog',default=False, action='store_true', help="result in natural, not log space. default is log space")
-    parser.add_argument('--binmode', default='t', type=str, choices=['d','s','t'], help='method to determine bin size {d: Duanes formula, s: n^1/2, t: 2*n**1/3(default)}')
+    parser.add_argument('--binmode', default='t', type=str, choices=['d','s','t','bb'], help='method to determine bin size {d: Duanes formula, s: n^1/2, t: 2*n**1/3(default), Bayesian blocks}')
     parser.add_argument('--path',   default=None, type=str, help="input/output path (must contain the input _max.txt and _min.txt files in a subdirectory sn_data)")
+    parser.add_argument('--unpickle',   default=False, action='store_true', help="read the pickled realization instead of making a new one")
+
     parser.add_argument('--verbose',default=False, action='store_true', help="verbose mode")
 
     args=parser.parse_args()
@@ -358,22 +351,25 @@ def main():
     CLOBBER=args.clobber
     VERBOSE=args.verbose
     BINMODE=args.binmode
+
+    if args.unpickle and NOPICKLE:
+        args.unpickle = False
+        print "cannot use pickle on this machine, wont save and wont read saved realizations. Ctr-C to exit, Return to continue?\n"
+        raw_input();
+
     if args.path:
         path=args.path
     else:
-        assert (os.getenv("MCMetdata"))," pass a path or set up the environmental variable MCMetdata pointing to the path where the _min _max _med files live"
+        assert (os.getenv("MCMetdata"))," the _max, _min (and _med) data must live in a folder named sn_data. pass a path to the sn_data folder, or set up the environmental variable MCMetdata pointing to the path where sn_data lives "
         path=os.getenv("MCMetdata")
     assert(os.path.isdir(path)),"pass a path or set up the environmental variable MCMetdata pointing to the path where the _min _max _med files live"
 
-
-
-    if args.nsample>0:
-        fi=input_format(args.filename, path=path)
+    if args.nsample>=100:
+        fi=input_format(args.name, path=path)
         if fi!=-1:
-            run(fi,args.nsample,delog=args.delog)
-
+            run(fi,args.nsample,delog=args.delog, unpickle=args.unpickle)
     else:
-        print "nsample must be positive number"
+        print "nsample must be at least 100"
     
 if __name__ == "__main__":
     main()
